c1d4ffde9a00e7355efcb0e0f144559ec427bc0e
DoherNaher <namedotnet@gmail.com>
2020-03-19 05:38:26 +0300

    Add rest point /item

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ const  express =require('express');
 const app = express();
 
 app.get('/',(req,resp)=>{
-    resp.send('Hello')
+    resp.send('Main page')
 })
-
+app.get('/item',(req,resp)=>{
+    resp.send('Item page')
+})
 app.listen(process.env.PORT || 3000 ,()=>{
     console.log('Server started')
 })
